ce4f1e906d3fd1977aca27013f7ea5b369b47e63
kalp-cg <kalp.patel.codinggita@gmail.com>
2026-05-24 17:42:51 +0530

    updated backend deployed url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  turbopack: {
-    root: "/home/kalppatel/Desktop/stockMaster/frontend",
-  },
 };
 
 export default nextConfig;
