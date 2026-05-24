2f5fc4738c828435b5f5a93ed3c094bf7984395f
kalp-cg <kalp.patel.codinggita@gmail.com>
2026-05-24 18:24:20 +0530

    again fullfill requirement

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/favicon.svg',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
